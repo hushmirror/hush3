2b6aaf5fde228d166f90c7084b3e8751e17ec70f
Duke Leto <duke@leto.net>
2020-12-06 15:38:21 -0500

    Add copyright

--- src/pubkey.h
+++ src/pubkey.h
@@ -1,9 +1,9 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin Core developers
 // Copyright (c) 2017 The Zcash developers
+// Copyright (c) 2019-2020 The Hush developers
 // Distributed under the GPLv3 software license, see the accompanying
 // file COPYING or https://www.gnu.org/licenses/gpl-3.0.en.html
-
 /******************************************************************************
  * Copyright © 2014-2019 The SuperNET Developers.                             *
  *                                                                            *
@@ -18,14 +18,11 @@
  * Removal or modification of this copyright notice is prohibited.            *
  *                                                                            *
  ******************************************************************************/
-
 #ifndef HUSH_PUBKEY_H
 #define HUSH_PUBKEY_H
-
 #include "hash.h"
 #include "serialize.h"
 #include "uint256.h"
-
 #include <stdexcept>
 #include <vector>
 
