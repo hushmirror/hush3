9262fcc53c4ed07fd95b46ed99c41b33d3574ad8
Duke Leto <duke@leto.net>
2020-12-06 15:53:36 -0500

    update copyright

--- src/script/standard.h
+++ src/script/standard.h
@@ -1,8 +1,8 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin Core developers
+// Copyright (c) 2019-2020 The Hush developers
 // Distributed under the GPLv3 software license, see the accompanying
 // file COPYING or https://www.gnu.org/licenses/gpl-3.0.en.html
-
 /******************************************************************************
  * Copyright © 2014-2019 The SuperNET Developers.                             *
  *                                                                            *
@@ -17,15 +17,11 @@
  * Removal or modification of this copyright notice is prohibited.            *
  *                                                                            *
  ******************************************************************************/
-
 #ifndef HUSH_SCRIPT_STANDARD_H
 #define HUSH_SCRIPT_STANDARD_H
-
 #include "script/interpreter.h"
 #include "uint256.h"
-
 #include <boost/variant.hpp>
-
 #include <stdint.h>
 
 class CKeyID;
