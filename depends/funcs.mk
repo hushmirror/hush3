39e0096affb0505ca3db3cc1b8f7765e42352d7f
Duke Leto <duke@leto.net>
2020-12-08 17:43:42 -0500

    mo copyrights

--- depends/funcs.mk
+++ depends/funcs.mk
@@ -1,3 +1,6 @@
+# Copyright (c) 2016-2020 The Hush developers
+# Distributed under the GPLv3 software license, see the accompanying
+# file COPYING or https://www.gnu.org/licenses/gpl-3.0.en.html
 define int_vars
 #Set defaults for vars which may be overridden per-package
 $(1)_cc=$($($(1)_type)_CC)
