f75bcb3ed33b59f926f620b3edb2bc8411bf1d13
Duke Leto <duke@leto.net>
2021-01-24 13:05:40 -0500

    update tests

--- qa/rpc-tests/test_framework/test_framework.py
+++ qa/rpc-tests/test_framework/test_framework.py
@@ -87,11 +87,11 @@ class BitcoinTestFramework(object):
 
         parser = optparse.OptionParser(usage="%prog [options]")
         parser.add_option("--nocleanup", dest="nocleanup", default=False, action="store_true",
-                          help="Leave komodods and test.* datadir on exit or error")
+                          help="Leave binaries and test.* datadir on exit or error")
         parser.add_option("--noshutdown", dest="noshutdown", default=False, action="store_true",
-                          help="Don't stop komodods after the test execution")
+                          help="Don't stop nodes after the test execution")
         parser.add_option("--srcdir", dest="srcdir", default="../../src",
-                          help="Source directory containing komodod/komodo-cli (default: %default)")
+                          help="Source directory containing hushd/hush-cli (default: %default)")
         parser.add_option("--tmpdir", dest="tmpdir", default=tempfile.mkdtemp(prefix="test"),
                           help="Root directory for datadirs")
         parser.add_option("--tracerpc", dest="trace_rpc", default=False, action="store_true",
@@ -134,7 +134,7 @@ class BitcoinTestFramework(object):
             stop_nodes(self.nodes)
             wait_bitcoinds()
         else:
-            print("Note: komodods were not stopped and may still be running")
+            print("Note: hushds were not stopped and may still be running")
 
         if not self.options.nocleanup and not self.options.noshutdown:
             print("Cleaning up")
@@ -148,7 +148,7 @@ class BitcoinTestFramework(object):
             sys.exit(1)
 
 
-# Test framework for doing p2p comparison testing, which sets up some komodod
+# Test framework for doing p2p comparison testing, which sets up some hushd
 # binaries:
 # 1 binary: test binary
 # 2 binaries: 1 test binary, 1 ref binary
@@ -162,10 +162,10 @@ class ComparisonTestFramework(BitcoinTestFramework):
 
     def add_options(self, parser):
         parser.add_option("--testbinary", dest="testbinary",
-                          default=os.getenv("BITCOIND", "komodod"),
+                          default=os.getenv("BITCOIND", "hush-smart-chain"),
                           help="bitcoind binary to test")
         parser.add_option("--refbinary", dest="refbinary",
-                          default=os.getenv("BITCOIND", "komodod"),
+                          default=os.getenv("BITCOIND", "hush-smart-chain"),
                           help="bitcoind binary to use for reference nodes (if any)")
 
     def setup_chain(self):
