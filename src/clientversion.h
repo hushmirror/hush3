b8701726714467de151047f555290062ed631205
Duke Leto <duke@leto.net>
2021-02-08 11:44:32 -0500

    bump ver and copyright year

--- src/clientversion.h
+++ src/clientversion.h
@@ -26,11 +26,11 @@
 
 // client versioning and copyright year
 //! These need to be macros, as clientversion.cpp's and bitcoin*-res.rc's voodoo requires it
-// Must be kept in sync with configure.ac !
+// Must be kept in sync with configure.ac , ugh!
 #define CLIENT_VERSION_MAJOR 3
 #define CLIENT_VERSION_MINOR 6
-#define CLIENT_VERSION_REVISION 1
-#define CLIENT_VERSION_BUILD 25
+#define CLIENT_VERSION_REVISION 2
+#define CLIENT_VERSION_BUILD 50
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
@@ -39,7 +39,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2020
+#define COPYRIGHT_YEAR 2021
 
 #endif //HAVE_CONFIG_H
 
